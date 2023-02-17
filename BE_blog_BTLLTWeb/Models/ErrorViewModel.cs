5002edb95c4a6d7bdf3d9c7e4191b9e3618ff03f
Duc <mintduc2612@gmail.com>
2023-02-17 21:36:48 +0700

    first commit

---
+++ BE_blog_BTLLTWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BE_blog_BTLLTWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
