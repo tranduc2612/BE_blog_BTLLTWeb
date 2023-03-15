2d1843739299f4da5097b2428a1a5da0fad7d59f
Duc <mintduc2612@gmail.com>
2023-03-15 21:11:47 +0700

    add create post view

---
+++ BE_blog_BTLLTWeb/Controllers/AuthController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BE_blog_BTLLTWeb.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+
+        public IActionResult Logout()
+        {
+            return View();
+        }
+    }
+}
---
+++ BE_blog_BTLLTWeb/Controllers/UserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BE_blog_BTLLTWeb.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
